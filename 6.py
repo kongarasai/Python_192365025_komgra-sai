c04ab6d635f07b83ca6b4a53168c100290b2971b
kongarasai <162015337+kongarasai@users.noreply.github.com>
2024-04-05 20:21:43 +0530

    Add files via upload
    enter the numnber5
    enter the numnber7
    enter the numnber3
    b is grater

---
+++ 6.py
@@ -0,0 +1,13 @@
+a=int(input("enter the numnber"))
+b=int(input("enter the numnber"))
+c=int(input("enter the numnber"))
+if(a>b):
+  if(a>c):
+    print("A is grater")
+  else:
+    print("c is greater")
+else:
+  if(b>c):
+    print("b is grater")
+  else:
+    print("c is greater")
